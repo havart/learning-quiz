2d0c002c569d9d3f7ad6f0ce89725c27d5bc3208
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-02-28 12:43:43 +0200

    fix: immutable data + prettier

--- frontend/src/app/components/smart/admin-all-questions/admin-all-questions.component.ts
+++ frontend/src/app/components/smart/admin-all-questions/admin-all-questions.component.ts
@@ -1,13 +1,13 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatPaginator, MatSort} from '@angular/material';
-import {QuestionHttpService} from 'src/app/services/question/question-http.service';
-import {IQuestion, QUESTION_CATEGORIES} from '../../../models/question.model';
-import {AdminAllQuestionsDataSource} from './admin-all-questions-datasource';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { QuestionHttpService } from 'src/app/services/question/question-http.service';
+import { IQuestion, QUESTION_CATEGORIES } from '../../../models/question.model';
+import { AdminAllQuestionsDataSource } from './admin-all-questions-datasource';
 
 @Component({
   selector: 'app-admin-all-questions',
   templateUrl: './admin-all-questions.component.html',
-  styleUrls: ['./admin-all-questions.component.css'],
+  styleUrls: ['./admin-all-questions.component.css']
 })
 export class AdminAllQuestionsComponent implements OnInit {
   listQuestions: IQuestion[];
@@ -17,30 +17,31 @@ export class AdminAllQuestionsComponent implements OnInit {
   displayedColumns = ['question', 'category', 'button'];
   QUESTION_CATEGORIES = QUESTION_CATEGORIES;
 
-  constructor(private questionService: QuestionHttpService) {
-  }
+  constructor(private questionService: QuestionHttpService) {}
 
   ngOnInit() {
     this.loadAllQuestions();
   }
 
   deleteById(id) {
-    this.questionService.delete(id)
-      .subscribe(
-        () => this.loadAllQuestions(),
-      );
+    this.questionService.delete(id).subscribe(() => this.loadAllQuestions());
   }
 
   loadAllQuestions() {
-    this.questionService.getAll()
-      .subscribe((results: IQuestion[]) => {
+    this.questionService.getAll().subscribe(
+      (results: IQuestion[]) => {
         this.listQuestions = results;
-      }, (err) => console.log('err' + err));
-    this.dataSource = new AdminAllQuestionsDataSource(this.paginator, this.sort);
+      },
+      err => console.log('err' + err)
+    );
+    this.dataSource = new AdminAllQuestionsDataSource(
+      this.paginator,
+      this.sort
+    );
   }
 
   getCategoryTitle(categoryId: number): string {
-    const category = QUESTION_CATEGORIES.find(el => (el.key === categoryId));
+    const category = QUESTION_CATEGORIES.find(el => el.key === categoryId);
 
     return category ? category.title : '-';
   }
