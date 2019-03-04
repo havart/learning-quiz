6e0c93b5b56f2d8a6c27df674e56ea329e87c517
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-03-04 10:31:17 +0200

    add: new component display on main page

--- frontend/src/app/components/dumb/category/category.component.ts
+++ frontend/src/app/components/dumb/category/category.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class CategoryComponent {
+@Input() testCategory;
 
 }
