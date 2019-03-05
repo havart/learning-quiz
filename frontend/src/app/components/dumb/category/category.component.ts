e2ffd60b16ae7536f2dfb9b1b7a2825f1f65eefa
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-03-05 08:58:26 +0200

    add: click on button

--- frontend/src/app/components/dumb/category/category.component.ts
+++ frontend/src/app/components/dumb/category/category.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { IQuiz } from 'src/app/models/quiz.model';
 
 @Component({
   selector: 'app-category',
@@ -6,7 +13,12 @@ import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
-@Input() testCategory;
+export class CategoryComponent implements OnInit {
+  @Input() testCategory;
 
+  constructor(private router: Router) {}
+  ngOnInit() {}
+  goById(id) {
+    this.router.navigate(['/test', id]);
+  }
 }
