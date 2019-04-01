ba25e9a9ae2d86cc7641bf8294985d74e93b3d91
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-04-01 17:38:41 +0300

    fix problem

--- frontend/src/app/components/smart/dropdown-form/dropdown-form.component.ts
+++ frontend/src/app/components/smart/dropdown-form/dropdown-form.component.ts
@@ -4,7 +4,7 @@ import { QUESTION_CATEGORIES, IQuestion } from '../../../models/question.model';
 
 import { Observable } from 'rxjs';
 import { QuestionHttpService } from 'src/app/services/question/question-http.service';
-import { map } from 'rxjs/operators';
+import { map, tap} from 'rxjs/operators';
 
 @Component({
     selector: 'app-dropdown-form',
@@ -15,17 +15,17 @@ export class DropdownFormComponent implements OnInit {
     form: FormGroup = new FormGroup({});
     QUESTION_CATEGORIES = QUESTION_CATEGORIES;
     listQuestions$: Observable<IQuestion[]>;
-    filterAllQuestions: Array<any>;
-    index: number;
+    filterAllQuestions$: Observable<IQuestion[]>;
+    id: string;
     isSave = false;
 
     constructor(private formBuilder: FormBuilder, private questionService: QuestionHttpService) {}
 
     ngOnInit() {
         this.initForm();
-        this.listQuestions$ = this.questionService.getAll();
     }
     private initForm() {
+        this.id = undefined;
         this.form = this.formBuilder.group({
             category: [null, [Validators.required]],
             question: [null, [Validators.required, Validators.minLength(3)]],
@@ -38,45 +38,22 @@ export class DropdownFormComponent implements OnInit {
     private loadQuestionById(questionId: string) {
         this.questionService.getByParam<IQuestion>(questionId).subscribe(question => this.form.patchValue(question));
     }
-    cancel() {
-        this.index = undefined;
-        this.form = this.formBuilder.group({
-            category: [null],
-            question: [null],
-            answer1: [null],
-            answer2: [null],
-            answer3: [null],
-            answer4: [null],
-        });
+
+    search() {
+        this.filterAllQuestions$ = this.questionService.getAll().pipe(
+            map(val => val.filter(prop => prop.category === this.form.value.category)),
+        );
     }
-    search(event) {
-        this.listQuestions$
-            .pipe(
-                map(val =>
-                    val.filter(prop => {
-                        if (prop.question.toLowerCase().includes(event.target.value.toLowerCase()) 
-                        && prop.category === this.form.value.category) {
-                            return prop;
-                        }
-                        if (event.target.value === null && prop.category === this.form.value.category) {
-                            return prop;
-                        }
-                    }),
-                ),
-            )
-            .subscribe(val => (this.filterAllQuestions = val));
-        if (this.form.value.question !== this.filterAllQuestions[i].question) {
-            this.index = undefined;
-        }
-    }
-    checkQuestion(i) {
-        this.loadQuestionById(this.filterAllQuestions[i]._id);
-        this.index = i;
+    checkQuestion(id) {
+        this.loadQuestionById(id);
+        this.id = id;
     }
     updateQuestion() {
-        if (this.index !== undefined) {
-            this.questionService.update(this.filterAllQuestions[this.index]._id, this.form.value).subscribe();
-            this.saved();
+        if (this.form.valid) {
+            this.questionService
+                .update(this.id, this.form.value)
+                .pipe(tap(() => this.saved()))
+                .subscribe();
         }
     }
     saved() {
