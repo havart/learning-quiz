51dd213482575f2df5a3489fb5c57bf40caec70a
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-04-05 09:06:46 +0300

    add button and modify form

--- frontend/src/app/components/smart/dropdown-form/dropdown-form.component.ts
+++ frontend/src/app/components/smart/dropdown-form/dropdown-form.component.ts
@@ -1,31 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { QUESTION_CATEGORIES, IQuestion } from '../../../models/question.model';
 import { Observable } from 'rxjs';
 import { QuestionHttpService } from 'src/app/services/question/question-http.service';
 import { map } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material';
-
+import { CallService, Call } from 'src/app/services/call/call.service';
 @Component({
     selector: 'app-dropdown-form',
     templateUrl: './dropdown-form.component.html',
     styleUrls: ['./dropdown-form.component.css'],
 })
-export class DropdownFormComponent implements OnInit {
+export class DropdownFormComponent implements OnInit, DoCheck {
     form: FormGroup = new FormGroup({});
     QUESTION_CATEGORIES = QUESTION_CATEGORIES;
-    filterAllQuestions$: Observable<IQuestion[]>;
+    filterAllQuestions$: Observable<any>;
+    currentCall$: Observable<Call>;
     id: string;
     isSave = false;
+    close = false;
 
     constructor(
         private formBuilder: FormBuilder,
         private questionService: QuestionHttpService,
         private snackBar: MatSnackBar,
+        private call: CallService,
     ) {}
 
     ngOnInit() {
         this.initForm();
+        this.currentCall$ = this.call.getCurrentCall$();
+    }
+    ngDoCheck() {
+        if (this.close) {
+            this.call.fillForm(this.form.value);
+        }
+    }
+    isOpen() {
+        this.close = !this.close;
+        this.currentCall$
+            .pipe(
+                map(val => {
+                    if (val) {
+                        this.form.patchValue(this.call.getCurrentQuestion());
+                    } else {
+                        this.initForm();
+                    }
+                }),
+            )
+            .subscribe();
     }
     private initForm() {
         this.id = undefined;
@@ -44,7 +67,7 @@ export class DropdownFormComponent implements OnInit {
     search() {
         this.filterAllQuestions$ = this.questionService
             .getAll()
-            .pipe(map(val => val.filter(prop => prop.category === this.form.value.category)));
+            .pipe(map(val => val.filter((prop: IQuestion) => prop.category === this.form.value.category)));
     }
     checkQuestion(id) {
         this.loadQuestionById(id);
