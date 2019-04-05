1b896d21d68fb3526225228f959be477238017b9
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-04-05 08:58:08 +0300

    add service

---
+++ frontend/src/app/services/call/call.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { IQuestion } from 'src/app/models/question.model';
+import { Subject, Observable } from 'rxjs';
+
+export interface Call {
+    phone: number;
+}
+@Injectable({
+    providedIn: 'root',
+})
+export class CallService {
+    buttonClick;
+    currentCall: Call = {} as Call;
+    private currentCall$ = new Subject<Call>();
+    private currentQuestion: IQuestion;
+
+    constructor() {}
+
+    makeCall(call: Call) {
+        this.currentCall$.next(call);
+    }
+    getCurrentCall$(): Observable<Call> {
+        return this.currentCall$;
+    }
+    getCurrentQuestion(): IQuestion {
+        return this.currentQuestion;
+    }
+    fillForm(form) {
+        this.currentQuestion = form;
+    }
+    completeCall() {
+        this.currentCall$.next(null);
+    }
+}
